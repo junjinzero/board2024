00ecf3bc247ef39440467543750976a5c5bff68c
junjinzero <progressnull@gmail.com>
2024-01-29 16:13:12 +0900

    #8. 글 작성 처리 ```java public String boardWritePro(String title, String content) // error public String boardWritePro(@RequestParam(name = "title") String title, @RequestParam(name = "content") String content) // success ```
    - @RequestParam 를 사용하면 요청 매개변수(html)와 메서드 매개변수(java) 간의 매핑을 정의하는 보다 명시적인 방법이 제공되어, 매개변수 바인딩 오류를 방지한다.
    - @Entity : 테이블 명과 동일하게 만드는 것이 좋으며, 테이블을 의미함
    - @Service : 실제로 로직을 처리하는 기능들을 명시
    - @Repository : JpaRepository 를 상속하는 Repository 생성, <사용하려는 Entity명, 해당 Entity의 PK의 타입>
    - 각 java 에서는 사용하려는 기능들을 @Autowired 로 주입해준다.

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,14 +1,27 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/board/write") //  웹 주소
     public String boardWriteFrom(){
         return "board_write";
+    }
+
+    @PostMapping("board/writepro")
+    public String boardWritePro(@ModelAttribute Board board) {
+        System.out.println("boardWriteProd");
+        boardService.write(board);
+
+        return "";
     }
 }
