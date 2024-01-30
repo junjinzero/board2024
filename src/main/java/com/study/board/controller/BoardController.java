cb7d54ed0441dbbc618f0993a2c45a50c47a5c3d
junjinzero <progressnull@gmail.com>
2024-01-30 21:58:23 +0900

    ### #9. 게시글 리스트 ```sql CREATE PROCEDURE board.testDataInsert() BEGIN     DECLARE i INT DEFAULT 1;
        WHILE i <= 120 DO
            INSERT INTO board(board_title, board_cont)
              VALUES(concat('제목',i), concat('내용',i));
            SET i = i + 1;
        END WHILE;
    END
    ```
    
    프로시저를 만들고
    
    call testDataInsert;
    
    데이터를 만들어준다.
    
    - findAll() : 데이터를 전부 보내줌
    - model.addAttribute 로 list 라는 이름으로, boardService.boardList() 의 내용을 보낼거다
    
    list 로 넣어준 값을 board 라는 명칭을 사용해서 써줄거다

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -23,5 +24,13 @@ public class BoardController {
         boardService.write(board);
 
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "board_list";
     }
 }
