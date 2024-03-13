a6bc5a5baa08553f8e4105e26bd91d1b933213e3
junjinzero <progressnull@gmail.com>
2024-03-13 17:42:06 +0900

    회원가입 validation 어노테이션 추가

--- src/main/java/com/study/board/controller/UserController.java
+++ src/main/java/com/study/board/controller/UserController.java
@@ -14,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Date;
+
 @Controller
 public class UserController {
 
@@ -30,6 +32,9 @@ public class UserController {
     @PostMapping("user/writepro")  // 회원가입
     public String userWritePro(User user) throws Exception {
         System.out.println("userWriteProd");
+
+        user.setUser_pw_chg_dttm(new Date());
+        user.setUse_yn("Y");
 
         userService.write(user);
 
--- src/main/java/com/study/board/entity/User.java
+++ src/main/java/com/study/board/entity/User.java
@@ -1,6 +1,8 @@
 package com.study.board.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.util.Date;
@@ -11,13 +13,30 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer user_key;
+
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
     private String user_id;
 //    @Column(name = "boardTitle")
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String user_nm;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String user_pw;
+
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$",
+            message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String user_nicknm;
+
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$",
+            message = "이메일 형식이 올바르지 않습니다.")
     private String user_email;
+
+    @NotBlank(message = "휴대폰 번호는 필수 입력 값입니다.")
     private String user_hpn;
+
     private Date user_pw_chg_dttm;
     private String use_yn;
     private Integer login_fail_num;
